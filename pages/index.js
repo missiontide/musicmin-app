d655972c449aa3b3f9c26bc66e241de5d2f3940b
missiontide <patcoronel@missiontide.com>
2022-12-24 10:36:21 -0800

    meta tags

--- pages/index.js
+++ pages/index.js
@@ -9,6 +9,11 @@ function HomePage() {
         <React.StrictMode>
             <Head>
                 <title>musicmin.app | Instant Worship Slides</title>
+                <meta property="og:url" content="https://www.musicmin.app" />
+                <meta property="og:title" content="musicmin.app - Instant Worship Slides!" />
+                <meta property="og:description" content="Never type another worship powerpoint again!" />
+                <meta property="og:image" content="https://www.musicmin.app/social-media-card.png" />
+                <meta property="og:type" content="website" />
             </Head>
             <App />
         </React.StrictMode>

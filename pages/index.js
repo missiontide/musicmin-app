f565c95069def48d67d040a6577dc62cf8d0a3ac
missiontide <patcoronel@missiontide.com>
2022-12-24 10:43:46 -0800

    meta updates

--- pages/index.js
+++ pages/index.js
@@ -10,8 +10,8 @@ function HomePage() {
             <Head>
                 <title>musicmin.app | Instant Worship Slides</title>
                 <meta property="og:url" content="https://www.musicmin.app" />
-                <meta property="og:title" content="musicmin.app - Instant Worship Slides!" />
-                <meta property="og:description" content="Never type another worship powerpoint again!" />
+                <meta property="og:title" content="Instant Worship Slides" />
+                <meta property="og:description" content="Let us take care of the .ppt, you focus on worshipping." />
                 <meta property="og:image" content="https://www.musicmin.app/social-media-card.png" />
                 <meta property="og:type" content="website" />
             </Head>

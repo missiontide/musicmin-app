7757651ba228e290ce5f58d5230df1c66385386c
missiontide <patcoronel@missiontide.com>
2022-12-24 08:36:40 -0800

    styles updated

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
-import '../styles/SongSearchBar.css'
-import '../styles/App.css'
 import '../styles/globals.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import '../styles/App.css'
+import '../styles/SongSearchBar.css'
 
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }) {

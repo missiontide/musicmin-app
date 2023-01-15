66892409a0d4d0e30bfa2fab8520d7151a844acf
missiontide <patcoronel@missiontide.com>
2023-01-14 19:06:04 -0800

    Chords: move API call into comp. & apicall force-cache

--- app/songs/[slug]/page.js
+++ app/songs/[slug]/page.js
@@ -1,28 +1,18 @@
 import React from 'react';
 import App from "../../../components/App";
-import ApiWrapper from "../../../utils/ApiWrapper";
 import Chords from "../../../components/Chords";
 
 export const Song = async (props) => {
-    const songData = await getSong(props.params.slug);
-
     return (
         <React.StrictMode>
             <App
                 useChordsPageStyling={true}
             />
             <Chords
-                title={songData.title}
-                artist={songData.artist}
-                chords={songData.chords}
+                slug={props.params.slug}
             />
         </React.StrictMode>
     )
-}
-
-async function getSong(slug) {
-    // await new Promise(resolve => setTimeout(resolve, 2000));
-    return await ApiWrapper.getSong(slug);
 }
 
 export default Song
--- components/Chords.js
+++ components/Chords.js
@@ -2,16 +2,34 @@
 
 import styles from '../styles/Chords.module.css';
 
-import React from "react";
+import React, { useState, useEffect } from 'react';
+import ApiWrapper from "../utils/ApiWrapper";
 
 export default function Chords(props) {
+    const [songData, setSongData] = useState({});
+
+    // Initialize: get song data from API
+    useEffect(() => {
+        // need to define an async function
+        const getSongData = async () => {
+            const data = await ApiWrapper.getSong(props.slug);
+            setSongData(data);
+        };
+
+        getSongData().then()
+    }, [])
+
     return (
         <div>
             <div className={styles.chordsContainer}>
                 <div className={styles.chords}>
-                    <h4>{props.title} - {props.artist}</h4>
-                    <br/>
-                    {<div dangerouslySetInnerHTML={{ __html: props.chords }} />}
+                    {Object.keys(songData).length === 0 ? <h4>Loading Chords...</h4> :
+                        (<>
+                        <h4>{songData.title} - {songData.artist}</h4>
+                        <br/>
+                        <div dangerouslySetInnerHTML={{ __html: songData.chords }} />
+                        </>)
+                    }
                 </div>
             </div>
         </div>
